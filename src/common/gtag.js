9682fb37dcb031c2558c01f5eaf05ae1ae420789
ken999999999 <keLearning2021@outlook.com>
2023-12-07 19:29:45 -0500

    add back tag function

--- src/common/gtag.js
+++ src/common/gtag.js
@@ -1,7 +1,7 @@
 export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
 
 export const pageView = (title) => {
-  // window?.gtag("event", "page_view", {
-  //   page_title: title,
-  // })
+  window?.gtag("event", "page_view", {
+    page_title: title,
+  })
 }
